6ccb7348ec31262c61bec59e0c48dd533da8fceb
Ryusei Endo <riyu02180218@gmail.com>
2023-10-07 20:44:07 +0900

    味方機能つけた

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 import time
+#========================================================================================================    クラス
 class Player():#味方クラス(1人)
     def __init__(self,hp,at,df,name,speed):
         self.hp = hp
@@ -13,75 +14,114 @@ class Enemy():#敵のクラス(1人)
         self.df = df
         self.name = name
         self.speed = speed
-
-def sort(enys):
+#========================================================================================================    ソート
+def enySort(enys):
     n = len(enys)
     for i in range(n):
         for j in range(0, n-i-1):
             if enys[j].speed < enys[j+1].speed:
                 enys[j], enys[j+1] = enys[j+1], enys[j]
-
-        
-def turn(iCount,enys,plys):
-    switchFlag = 0
-    #--------敵のターン------------------
-    time.sleep(0.5)
-    print("---相手のターン-------------")
-    print(enys[iCount].name,"の攻撃")
-    damage = max(enys[iCount].at-plys[0].df,0)
-    plys[0].hp -= damage
-    print(enys[iCount].name,"は",plys[0].name,"に",damage,"ダメージを負わせた！")
-    print("残りの",plys[0].name,"のhpは",plys[0].hp)
-    #print(iCount)
-    if iCount == 3:
-        switchFlag = 1
-        iCount = 0
-    #---------------------------------------------------------------------------
-    if switchFlag == 1:
-        switchFlag = 0
-        if plys[0].hp >= 1:
-            time.sleep(1)
+def plySort(plys):
+    n = len(plys)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if plys[j].speed < plys[j+1].speed:
+                plys[j], plys[j+1] = plys[j+1], plys[j]
+def enyHpSort(enys):
+    n = len(enys)#敵ソート用
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if enys[j].hp > enys[j+1].hp:
+                enys[j], enys[j+1] = enys[j+1], enys[j]
+def plyHpSort(plys):
+    n = len(plys)#味方ソート用
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if plys[j].hp > plys[j+1].hp:
+                plys[j], plys[j+1] = plys[j+1], plys[j]
+#========================================================================================================    ターン
+def enyTurn(enys,plys,end):#敵ターン関数
+    plyHpSort(plys)
+    for eny in enys:
+        time.sleep(0.2)
+        print("---相手のターン-------------")
+        #print("i",i,len(enys)-1)
+        print(eny.name,"の攻撃")#←攻撃↓
+        damage = max(eny.at-plys[0].df,0)
+        plys[0].hp -= damage
+        print("残りの",plys[0].name,"のhpは",plys[0].hp)
+        print(eny.name,"は",plys[0].name,"に",damage,"ダメージを負わせた！")
+        if plys[0].hp <= 0:
+            plys.pop(0)
+        if len(plys) == 0:
+            print("敵チームの勝利")
+            end = 1
+            break
+    return enys,plys,end
+#==============================================================================
+def plyTurn(enys,plys,end):#味方ターン関数
+    enyHpSort(enys)
+    for ply in plys:
+        if ply.hp >= 1:
+            time.sleep(0.5)
             #--------味方のターン---------------
             print("---味方のターン-------------")
-            print(plys[0].name,"の攻撃")
-            damage = max(plys[0].at-enys[iCount].df,0)
-            enys[iCount].hp -= damage
-            print(plys[0].name,"は",enys[iCount].name,"に",damage,"ダメージを負わせた！")
-            print("残りの",enys[iCount].name,"のhpは",enys[iCount].hp)
-    return enys,plys
+            print(ply.name,"の攻撃")
+            damage = max(ply.at-enys[0].df,0)
+            enys[0].hp -= damage
+            print(ply.name,"は",enys[0].name,"に",damage,"ダメージを負わせた！")
+            print("残りの",enys[0].name,"のhpは",enys[0].hp)
+            if enys[0].hp <= 0:
+                enys.pop(0)
+            if len(enys) == 0:
+                print("味方チームの勝利")
+                end = 1
+                break
+    return enys,plys,end
+#========================================================================================================   メイン関数 
 def main():
+    end = 0#Whileの強制終了
+    turnCount = 0#ターン数
     plys = []#プレイヤーをまとめた配列
-    ply1 = Player(1000,50,0,"自分",15)
-    ply2 = Player(80,40,10,"猫",20)
+    ply1 = Player(150,60,30,"自分",15)
+    ply2 = Player(90,70,30,"猫",20)
     ply3 = Player(200,50,40,"巨人",5)
-    ply4 = Player(100,50,20,"自分のクローン",10)
+    ply4 = Player(120,40,20,"自分のクローン",10)
     plys.append(ply1)
     plys.append(ply2)
     plys.append(ply3)
     plys.append(ply4)
     enys = []#敵をまとめた配列
-    eny1 = Enemy(150,30,30,"鬼",5)
-    eny2 = Enemy(100,50,10,"青鬼",10)
-    eny3 = Enemy(80,50,0,"ネズミ",20)
-    eny4 = Enemy(30,50,40,"チビ",15)
+    eny1 = Enemy(200,50,30,"鬼",5)
+    eny2 = Enemy(200,70,10,"青鬼",10)
+    eny3 = Enemy(50,30,40,"ネズミ",20)
+    eny4 = Enemy(80,20,10,"チビ",15)
     enys.append(eny1)
     enys.append(eny2)
     enys.append(eny3)
     enys.append(eny4)
-    while True:
-        sort(enys)
-        print("ターンの境目--------------------------------------------")
-        for i in range(len(enys)):
-            enys,plys = turn(i,enys,plys)
-            if enys[i].hp <= 0:
-                plys.pop(0)
-        if plys[0].hp <= 0:
-            print("敵チームの勝利")
-            break
-        if enys ==  "":
-            print("味方チームの勝利")
+    while end == 0:
+        turnCount += 1
+        time.sleep(0.25)
+        print("ターンの境目--------------------------------------------",turnCount,"ターン目")
+        #for eny in enys:
+        for eny in enys:
+            print("eny",eny.name,eny.hp)
+        for ply in plys:
+            print("ply",ply.name,ply.hp)
+
+        #敵ターン-----
+        enySort(enys)
+        enys,plys,end = enyTurn(enys,plys,end)
+        #味方ターン---
+        plySort(plys)
+        enys,plys,end = plyTurn(enys,plys,end)
+
+
+
+            
 main()
 
         
         
-
+ 
