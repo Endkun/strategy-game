7624a3320f43c771dbdacb3efefff5a9724b2762
Ryusei Endo <riyu02180218@gmail.com>
2023-11-18 20:17:54 +0900

    移動1

--- Story1.py
+++ Story1.py
@@ -4,10 +4,12 @@ import sys
 import random
 import time
 class Character():
-    def __init__(self,x,y,CharacterType,Image,Team,Name):#-----------------------------------------------------------初期化
+    def __init__(self,x,y,CharacterType,Image,Team,Name,font2):#-----------------------------------------------------------初期化
         self.x = x
         self.y = y
+        self.font2 = font2
         self.tick = 0
+        self.isBUTTONDOWN = False
         self.Image = Image#イメージ画像
         self.Team = Team#チーム   味方チーム、敵チーム、モブチームOnly
         self.Name = Name#名前
@@ -48,19 +50,51 @@ class Character():
             if self.Name == "Cat":
                 if self.tick == 400:
                     self.y += 1
+                if self.tick == 550:
+                    self.x += 2
                 if self.tick == 650:
                     self.y += 4
+    def button(self,screen,mapchip):#移動ボタン用
+        if self.isBUTTONDOWN == True:
+            if mapchip[self.y-1][self.x] == "1": #上
+                pygame.draw.circle(screen,(250,250,0),((self.x+0.5)*100,(self.y-0.5)*100),10)
+            if mapchip[self.y+1][self.x] == "1": #下  
+                pygame.draw.circle(screen,(250,250,0),((self.x+0.5)*100,(self.y+1.5)*100),10) 
+            if mapchip[self.y][self.x+1] == "1": #右
+                pygame.draw.circle(screen,(250,250,0),((self.x+1.5)*100,(self.y+0.5)*100),10)
+            if mapchip[self.y][self.x-1] == "1": #左
+                pygame.draw.circle(screen,(250,250,0),((self.x-0.5)*100,(self.y+0.5)*100),10) 
+        if self.tick >= 700:
+            pygame.draw.rect(screen, (255,255,255), Rect(150,700,200,100))
+            txt = self.font2.render("移動", True, (0,0,0))   # 描画する文字列の設定
+            screen.blit(txt, [190, 720])# 文字列の表示位置
+        # イベント処理
+        for event in pygame.event.get():  # イベントキューからキーボードやマウスの動きを取得
+            if event.type == QUIT:        # 閉じるボタンが押されたら終了
+                pygame.quit()             # Pygameの終了(ないと終われない)
+                sys.exit()                # 終了（ないとエラーで終了することになる）
+            elif event.type == MOUSEBUTTONDOWN:
+                if self.tick >= 700:
+                    if self.isBUTTONDOWN == False:
+                        btn = event.button
+                        x, y = event.pos
+                        if x > 150 and x < 350:
+                            if y > 700 and y < 800:
+                                self.isBUTTONDOWN = True   
     def draw(self,screen):#-----------------------------------------------------------描画
         screen.blit(self.Image,Rect(self.x*100,self.y*100,50,50))
  
 
 def main():#-----------------------------------------------------------メイン
     pygame.init()        
-    font = pygame.font.SysFont("yumincho", 30)                       # Pygameの初期化
+    font = pygame.font.SysFont("yumincho", 30)       
+    font2 = pygame.font.SysFont("yumincho", 60)                       
     screen = pygame.display.set_mode((500, 900))  # 800
     pt1 = pygame.image.load("PlotTile1.png").convert_alpha()   #配置タイル 全て100x100
     pt2 = pygame.image.load("PlotTile2.png").convert_alpha()   #モブタイル　
-    pt3 = pygame.image.load("PlotTile3.png").convert_alpha()   #配置タイル 全て100x100
+    pt3 = pygame.image.load("PlotTile3.png").convert_alpha()   #字幕タイル
+    door = pygame.image.load("door.png").convert_alpha()   #ドアタイル
+    door2 = pygame.image.load("door2.png").convert_alpha()   #裏口タイル
     Pl1 = pygame.image.load("player1.png").convert_alpha()       #プレイヤー
     Pl2 = pygame.image.load("player2.png").convert_alpha()       #プレイヤー
     Cat = pygame.image.load("cat.png").convert_alpha()       #プレイヤー
@@ -70,20 +104,23 @@ def main():#-----------------------------------------------------------メイン
     tx = 0#タイル用x,y
     ty = 0
     tick = 0
-    player1 = Character(2,5,"Player",Pl1,"味方","Player")
-    player2 = Character(3,4,"Player",Pl2,"味方","Mikata1")
-    slime1 = Character(-1,0,"Slime",Sl1,"敵","BlueSlime")
-    slime2 = Character(-1,0,"Slime",Sl2,"敵","GreenSlime")
-    goutou = Character(-1,0,"Goutou",Man,"敵","Yakuza Sumiyoshi")
-    cat = Character(1,4,"Animal",Cat,"モブ","Cat")
+    player1 = Character(2,5,"Player",Pl1,"味方","Player",font2)
+    player2 = Character(3,4,"Player",Pl2,"味方","Mikata1",font2)
+    slime1 = Character(-1,0,"Slime",Sl1,"敵","BlueSlime",font2)
+    slime2 = Character(-1,0,"Slime",Sl2,"敵","GreenSlime",font2)
+    goutou = Character(-1,0,"Goutou",Man,"敵","Yakuza Sumiyoshi",font2)
+    cat = Character(1,4,"Animal",Cat,"モブ","Cat",font2)
+    players = [player1]
+    enemys = [slime1,slime2,goutou]
+    mobs = [cat,player2]
     mapchip = [
         ["2","2","2","2","2"],
         ["0","0","0","0","0"],
-        ["0","0","0","0","0"],
+        ["0","0","3","0","0"],
         ["0","1","1","1","0"],
         ["0","1","1","1","0"],
         ["0","1","1","1","0"],
-        ["0","0","0","0","0"],
+        ["0","0","0","4","0"],
         ["0","0","0","0","0"],
         ["0","0","0","0","0"],
         ]
@@ -100,6 +137,10 @@ def main():#-----------------------------------------------------------メイン
                     screen.blit(pt2 ,Rect(tx+i*100,ty+j*100,50,50))  
                 elif mapchip[j][i] == "2":
                     screen.blit(pt3 ,Rect(tx+i*100,ty+j*100,50,50))
+                elif mapchip[j][i] == "3":
+                    screen.blit(door ,Rect(tx+i*100,ty+j*100,50,50))
+                elif mapchip[j][i] == "4":
+                    screen.blit(door2,Rect(tx+i*100,ty+j*100,50,50))
         if tick < 500:
             Story = font.render("喫茶店でくつろいでいたら", True, (0,0,255)) # 描画する文字列を画像にする
             Story2 = font.render("突然強盗が入ってきた！", True, (0,0,255)) # 描画する文字列を画像にする
@@ -109,26 +150,24 @@ def main():#-----------------------------------------------------------メイン
         screen.blit(Story, [70,40])
         screen.blit(Story2,[70,70])   
         #---------アニメーション---------
-        goutou.firstAnimation()
-        slime1.firstAnimation()     
-        slime2.firstAnimation()
-        player1.firstAnimation()
-        player2.firstAnimation()
-        cat.firstAnimation()
+        for player in players:
+            player.firstAnimation()
+        for enemy in enemys:
+            enemy.firstAnimation()
+        for mob in mobs:
+            mob.firstAnimation()
+
+        #---------プレイヤー-------------------
+        for player in players:
+            player.button(screen,mapchip)
         #---------描画---------
-        goutou.draw(screen)
-        slime1.draw(screen)
-        slime2.draw(screen)
-        player1.draw(screen)
-        player2.draw(screen)
-        cat.draw(screen)
+        for player in players:
+            player.draw(screen)
+        for enemy in enemys:
+            enemy.draw(screen)
+        for mob in mobs:
+            mob.draw(screen)
                           
         pygame.display.update()         
         ck.tick(60) #1秒間で30フレームになるように33msecのwait                           
-        # イベント処理
-        for event in pygame.event.get():  # イベントキューからキーボードやマウスの動きを取得
-            if event.type == QUIT:        # 閉じるボタンが押されたら終了
-                pygame.quit()             # Pygameの終了(ないと終われない)
-                sys.exit()                # 終了（ないとエラーで終了することになる）
-if __name__ == "__main__":
-    main()
+main()
