800209da1f074965a9a64a2bfff712911ba07049
bkh4149 <bkh4149@gmail.com>
2023-11-25 23:40:41 +0900

    バックグラウンドもクラス化した

--- Story1.py
+++ Story1.py
@@ -7,7 +7,7 @@ class Character():
     def __init__(self,x,y,CharacterType,Image,Team,Name,font2):#-----------------------------------------------------------初期化
         self.x = x
         self.y = y
-        self.action = [] #取り敢えず"Move"は動く "Fight"は戦う 
+        self.action = ["move"] #取り敢えず"Move"は動く "Fight"は戦う 
         self.font2 = font2
         self.tick = 0
         self.isBUTTONDOWN = "down"
@@ -57,34 +57,35 @@ class Character():
                     self.x += 2
                 if self.tick == 650:
                     self.y += 4
-    def update(self,screen,mapchip):#移動ボタン用
+    def update(self,screen,bacG):#移動ボタン用
     #----------------------------------------------------------------------------------------------------------移動アクション
-        if "Move" in self.action :
+        if "move" in self.action :
+            print("@53 move")
             #-----------------------------------------------------------------------------------------動ける所の描画
             if self.isBUTTONDOWN == "MoveLighton":#プレイヤーはx=2,y=5
-                if mapchip[self.y-1][self.x] == "1": #上
+                if bacG.mapchip[self.y-1][self.x] == "1": #上
                     pygame.draw.circle(screen,(250,250,0),((self.x+0.5)*100,(self.y-0.5)*100),10)
-                if mapchip[self.y+1][self.x] == "1": #下  
+                if bacG.mapchip[self.y+1][self.x] == "1": #下  
                     pygame.draw.circle(screen,(250,250,0),((self.x+0.5)*100,(self.y+1.5)*100),10)
-                if mapchip[self.y][self.x+1] == "1": #右
+                if bacG.mapchip[self.y][self.x+1] == "1": #右
                     pygame.draw.circle(screen,(250,250,0),((self.x+1.5)*100,(self.y+0.5)*100),10)
-                if mapchip[self.y][self.x-1] == "1": #左
+                if bacG.mapchip[self.y][self.x-1] == "1": #左
                     pygame.draw.circle(screen,(250,250,0),((self.x-0.5)*100,(self.y+0.5)*100),10)
             #------------------------------------------------------------------------------------------動ける所の検出
             if self.isBUTTONDOWN == "MoveLighton":
-                if mapchip[self.y-1][self.x] == "1": #上
+                if bacG.mapchip[self.y-1][self.x] == "1": #上
                     self.canMoveUp = True
                 else:
                     self.canMoveUp = False
-                if mapchip[self.y+1][self.x] == "1": #下  
+                if bacG.mapchip[self.y+1][self.x] == "1": #下  
                     self.canMoveDown = True
                 else:
                     self.canMoveDown = False
-                if mapchip[self.y][self.x+1] == "1": #右
+                if bacG.mapchip[self.y][self.x+1] == "1": #右
                     self.canMoveRight = True
                 else:
                     self.canMoveRight = False
-                if mapchip[self.y][self.x-1] == "1": #左
+                if bacG.mapchip[self.y][self.x-1] == "1": #左
                     self.canMoveLeft = True
                 else:
                     self.canMoveLeft = False
@@ -96,13 +97,15 @@ class Character():
                 txt = self.font2.render("移動", True, (0,0,0))   # 描画する文字列の設定
                 screen.blit(txt, [190, 720])# 文字列の表示位置
 #----------------------------------------------------------------------------------------------------------戦うアクション
-        if "Fight" in self.action: #--------------------------------------------------戦う
-            if mapchip[self.y-1][self.x] == "5" or mapchip[self.y+1][self.x] == "5" or mapchip[self.y][self.x+1] == "5" or mapchip[self.y][self.x-1] == "5":
+        if "fight" in self.action: #--------------------------------------------------戦う
+            print("@101 fight")
+
+            if bacG.mapchip[self.y-1][self.x] == "5" or bacG.mapchip[self.y+1][self.x] == "5" or bacG.mapchip[self.y][self.x+1] == "5" or bacG.mapchip[self.y][self.x-1] == "5":
                 pygame.draw.rect(screen, (255,255,255), Rect(150,700,200,100))
                 txt = self.font2.render("戦う", True, (0,0,0))   # 描画する文字列の設定
                 screen.blit(txt, [200, 720])# 文字列の表示位置
             else:
-                self.action = "Move"
+                self.action = "move"
                 self.isBUTTONDOWN = "isnotpushed"
         #-----------------------------------------------------------------------------------------イベント処理
         for event in pygame.event.get():  # イベントキューからキーボードやマウスの動きを取得
@@ -146,20 +149,40 @@ class Character():
         screen.blit(self.Image,Rect(self.x*100,self.y*100,50,50))
         
 
-def drawBackGround(screen,mapchip,enemys,tiles):
-    tx = 0#タイル用x,y
-    ty = 0
-    screen.fill((255,255,255))
-    for i in range(5):
-        for j in range(9):
-            for enemy in enemys:
-                if enemy.x == i and enemy.y == j:
-                    if mapchip[j][i] == "1":
-                        mapchip[j][i] = "5"
-            mapnum = int(mapchip[j][i])            
-            screen.blit(tiles[mapnum] ,Rect(tx+i*100,ty+j*100,50,50))            
+class BackGround():
+    def __init__(self):
+        pt1 = pygame.image.load("PlotTile1.png").convert_alpha()   #配置タイル 全て100x100
+        pt2 = pygame.image.load("PlotTile2.png").convert_alpha()   #モブタイル　
+        pt3 = pygame.image.load("PlotTile3.png").convert_alpha()   #字幕タイル
+        door = pygame.image.load("door.png").convert_alpha()   #ドアタイル
+        door2 = pygame.image.load("door2.png").convert_alpha()   #裏口タイル
+        self.tiles=[pt2,pt1,pt3,door,door2,pt1]
+        self.mapchip = [
+            ["2","2","2","2","2"],
+            ["0","0","0","0","0"],
+            ["0","0","3","0","0"],
+            ["0","1","1","1","0"],
+            ["0","1","1","1","0"],
+            ["0","1","1","1","0"],
+            ["0","0","0","4","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ]
 
-def opening(screen,font,mapchip,enemys,players,mobs,tiles):#--------------------
+    def draw(self,screen,enemys):
+        tx = 0#タイル用x,y
+        ty = 0
+        screen.fill((255,255,255))
+        for i in range(5):
+            for j in range(9):
+                for enemy in enemys:
+                    if enemy.x == i and enemy.y == j:
+                        if self.mapchip[j][i] == "1":
+                            self.mapchip[j][i] = "5"
+                mapnum = int(self.mapchip[j][i])            
+                screen.blit(self.tiles[mapnum] ,Rect(tx+i*100,ty+j*100,50,50))            
+
+def opening(screen,font,enemys,players,mobs,bacG):#--------------------
     ##オープニング
     ck = pygame.time.Clock()
     tick=0
@@ -168,7 +191,7 @@ def opening(screen,font,mapchip,enemys,players,mobs,tiles):#--------------------
         if tick>800:
             break
 
-        drawBackGround(screen,mapchip,enemys,tiles)
+        bacG.draw(screen,enemys)
 
         if tick <= 500:
             Story = font.render("喫茶店でくつろいでいたら", True, (0,0,255)) # 描画する文字列を画像にする
@@ -207,13 +230,7 @@ def main():#-----------------------------------------------------------メイン
     font2 = pygame.font.SysFont("yumincho", 60)                       
     screen = pygame.display.set_mode((500, 900))  # 800
     ck = pygame.time.Clock()
-    pt1 = pygame.image.load("PlotTile1.png").convert_alpha()   #配置タイル 全て100x100
-    pt2 = pygame.image.load("PlotTile2.png").convert_alpha()   #モブタイル　
-    pt3 = pygame.image.load("PlotTile3.png").convert_alpha()   #字幕タイル
-    door = pygame.image.load("door.png").convert_alpha()   #ドアタイル
-    door2 = pygame.image.load("door2.png").convert_alpha()   #裏口タイル
 
-    tiles=[pt2,pt1,pt3,door,door2,pt1]
     Pl1 = pygame.image.load("player1.png").convert_alpha()       #プレイヤー
     Pl2 = pygame.image.load("player2.png").convert_alpha()       #プレイヤー
     Cat = pygame.image.load("cat.png").convert_alpha()       #プレイヤー
@@ -229,27 +246,18 @@ def main():#-----------------------------------------------------------メイン
     players = [player1]
     enemys = [slime1,slime2,goutou]
     mobs = [cat,player2]
-    mapchip = [
-        ["2","2","2","2","2"],
-        ["0","0","0","0","0"],
-        ["0","0","3","0","0"],
-        ["0","1","1","1","0"],
-        ["0","1","1","1","0"],
-        ["0","1","1","1","0"],
-        ["0","0","0","4","0"],
-        ["0","0","0","0","0"],
-        ["0","0","0","0","0"],
-        ]
+    bacG=BackGround()
 
-    opening(screen,font,mapchip,enemys,players,mobs,tiles)
+
+    opening(screen,font,enemys,players,mobs,bacG)
 
     #バトルシーン　
     while True:
-        drawBackGround(screen,mapchip,enemys,tiles)
+        bacG.draw(screen,enemys)
 
         #---------プレイヤー-------------------
         for player in players:
-            player.update(screen,mapchip)
+            player.update(screen,bacG)
         #---------描画---------
         for player in players:
             player.draw(screen)
